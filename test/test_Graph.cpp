dfb1646853a4391b4f72acf22d61f15293103f7a
Thomas Roehr <thomas.roehr@dfki.de>
2013-12-06 18:46:10 +0100

    Update tests

--- test/test_Graph.cpp
+++ test/test_Graph.cpp
@@ -29,21 +29,39 @@ BOOST_AUTO_TEST_CASE(it_should_allow_applying_a_view)
     VertexData::Ptr vertexData1(new DirectedGraph::VertexProperty());
 
     graph.assignVertexProperty(v0, vertexData0);
+    BOOST_REQUIRE_MESSAGE( v0 == vertexData0->getVertex(&graph), "Retrieve assigned vertex");
+
     graph.assignVertexProperty(v1, vertexData1);
+    BOOST_REQUIRE_MESSAGE( v1 == vertexData1->getVertex(&graph), "Retrieve assigned vertex");
+
 
     EdgeData::Ptr edgeData0(new DirectedGraph::EdgeProperty());
-
     Edge e = graph.addEdge(v0,v1);
+
     graph.assignEdgeProperty(e, edgeData0);
+    BOOST_REQUIRE_MESSAGE(e == edgeData0->getEdge(&graph), "Assigned edge can be retrieved");
 
 
     // Set graph view
-    graph_analysis::GraphView<Graph,VertexData::Ptr, EdgeData::Ptr> gv;
+    {
+        graph_analysis::GraphView<Graph,VertexData::Ptr, EdgeData::Ptr> gv;
+        graph_analysis::Filter< VertexData::Ptr >::Ptr filter(new graph_analysis::filter::PermitAll< VertexData::Ptr >() );
+        gv.setVertexFilter(filter);
 
-    graph_analysis::Filter< VertexData::Ptr >::Ptr filter(new graph_analysis::Filter< VertexData::Ptr >() );
-    gv.setVertexFilter(filter);
+        DirectedSubGraph subGraph = gv.apply(graph);
+        int subgraphCount = ::lemon::countNodes(subGraph.raw());
+        int graphCount = ::lemon::countNodes(graph.raw());
+        BOOST_REQUIRE_MESSAGE( subgraphCount == graphCount, "Subgraph contains all nodes after applying PermitAll filter: " << subgraphCount << " vs. " << graphCount);
+    }
 
-    DirectedGraph::SubGraph subGraph = gv.apply(graph);
+    {
+        graph_analysis::GraphView<Graph,VertexData::Ptr, EdgeData::Ptr> gv;
+        graph_analysis::Filter< VertexData::Ptr >::Ptr filter(new graph_analysis::filter::DenyAll< VertexData::Ptr >() );
+        gv.setVertexFilter(filter);
 
-    ::lemon::countNodes(subGraph);
+        DirectedSubGraph subGraph = gv.apply(graph);
+
+        int subgraphCount = ::lemon::countNodes(subGraph.raw());
+        BOOST_REQUIRE_MESSAGE( subgraphCount == 0, "Subgraph contains no nodes after applying DenyAll filter" << subgraphCount << "vs. 0 " );
+    }
 }
