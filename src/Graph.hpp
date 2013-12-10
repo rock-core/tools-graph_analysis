c33389b2c4145dc75ffb9a7035a47e6ff80d7f4b
Thomas Roehr <thomas.roehr@dfki.de>
2013-12-10 09:51:33 +0100

    Add documentation

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -30,9 +30,11 @@ public:
      */
     virtual ~BaseGraph() {}
 
+
+    // Exporting the typenames so that they can be accesses
+    // via BaseGraphType namespace
     typedef VertexType Vertex;
     typedef EdgeType Edge;
-
     typedef BaseGraph<GraphType,VertexType,EdgeType> BaseGraphType;
     typedef GraphType RawGraphType;
 
@@ -90,6 +92,15 @@ protected:
     GraphType mGraph;
 };
 
+/**
+ * \brief BaseSubGraph represent a wrapper for libraries that use an invidual datatype for subgraphs
+ * \details This wrapper guarantees that vertex filters and edgefilters won't be deallocated
+ * as long as the subgraph is in use, e.g., for the lemon library the subgraph holds a reference to the main graph and only
+ * associates filters -- but since the filters are added as references they need to be managed properly.
+ * \tparam SubGraphType the underlying subgraph type
+ * \tparam VertexFilter the vertex filter
+ * \tparam EdgeFilter the edge filter
+ */
 template<typename SubGraphType, typename VertexFilter, typename EdgeFilter>
 class BaseSubGraph
 {
@@ -100,12 +111,25 @@ public:
     typedef VertexFilter VertexFilterType;
     typedef EdgeFilter EdgeFilterType;
 
+    /**
+     * This default Constructor will not(!) initialize the underlying subgraph type, but
+     * only the given filters. 
+     * Make sure that any class inheriting from this one properly initalizes the subgraph, i.e.
+     * using setSubgraph
+     *
+     * Ownership of all objects, i.e. filters and graph is transferred to this class, which internally
+     * uses shared pointer to guarantee cleanup when necessary
+     */
     BaseSubGraph(VertexFilter* vertexFilter, EdgeFilter* edgeFilter)
     {
         mpVertexFilter = boost::shared_ptr<VertexFilter>(vertexFilter);
         mpEdgeFilter = boost::shared_ptr<EdgeFilter>(edgeFilter);
     }
 
+    /**
+     * Retrieve the raw subgraph type. Use with care since this object's memory might
+     * be deallocated if you release your main object
+     */
     SubGraphType& raw()
     {
         assert(mpSubgraph);
@@ -113,18 +137,30 @@ public:
     }
 
 protected:
+    /**
+     * Get vertex filter
+     * \return reference to vertex filter
+     */
     VertexFilter& getVertexFilter() const
     {
         assert(mpVertexFilter);
         return *mpVertexFilter.get();
     }
 
+    /**
+     * Get edge filter
+     * \return reference to edge filter
+     */
     EdgeFilter& getEdgeFilter() const
     {
         assert(mpEdgeFilter);
         return *mpEdgeFilter.get();
     }
 
+    /**
+     * Set the subgraph
+     * \param pointer to subgraph
+     */
     void setSubgraph(SubGraphType* subgraph)
     {
         assert(subgraph);
