6c96eb392b59c015759bed7ff85c72c01bdf88b3
Thomas Roehr <thomas.roehr@dfki.de>
2013-12-11 12:02:36 +0100

    Add EdgeIterator and VertexIterator

---
+++ src/EdgeIterator.hpp
@@ -0,0 +1,42 @@
+#ifndef GRAPH_ANALYSIS_EDGE_ITERATOR_HPP
+#define GRAPH_ANALYSIS_EDGE_ITERATOR_HPP
+
+#include <graph_analysis/Edge.hpp>
+
+namespace graph_analysis {
+
+/**
+ * \brief EdgeIterator interface
+ * Allows to access all edges of a graph. Requires implementation
+ * to wrap the underlying graph library in use
+ */
+class EdgeIterator
+{
+public:
+    typedef boost::shared_ptr<EdgeIterator> Ptr;
+
+    /**
+     * \brief Test if there is a next item and if it is then updates
+     * the current to next
+     * \throws std::runtime_error if not implemented by subclass
+     */
+    virtual bool next() { throw std::runtime_error("EdgeIterator: next() has not been implemented"); }
+
+    /**
+     * Retrieve the current edge this iterator is pointing to
+     */
+    Edge::Ptr current() { return mEdge; }
+
+protected:
+    /**
+     * \brief Set next edge
+     * Should be applied by class implementing next()
+     */
+    void setNext(Edge::Ptr vertex) { mEdge = vertex; }
+
+private:
+    Edge::Ptr mEdge;
+};
+
+} // end namespace graph_analysis
+#endif // GRAPH_ANALYSIS_EDGE_ITERATOR_HPP
---
+++ src/VertexIterator.hpp
@@ -0,0 +1,42 @@
+#ifndef GRAPH_ANALYSIS_VERTEX_ITERATOR_HPP
+#define GRAPH_ANALYSIS_VERTEX_ITERATOR_HPP
+
+#include <graph_analysis/Vertex.hpp>
+
+namespace graph_analysis {
+
+/**
+ * \brief VertexIterator interface
+ * Allows to access all vertices of a graph. Requires implementation
+ * to wrap the underlyning graph library in use
+ */
+class VertexIterator
+{
+public:
+    typedef boost::shared_ptr<VertexIterator> Ptr;
+
+    /**
+     * \brief Test if there is a next item and if it is then updates
+     * the current to next
+     * \throws std::runtime_error if not implemented by subclass
+     */
+    virtual bool next() { return false; }
+
+    /**
+     * Retrieve the current vertex this iterator is pointing to
+     */
+    Vertex::Ptr current() { return mVertex; }
+
+protected:
+    /**
+     * \brief Set next vertex
+     * Should be applied by class implementing next()
+     */
+    void setNext(Vertex::Ptr vertex) { mVertex = vertex; }
+
+private:
+    Vertex::Ptr mVertex;
+};
+
+} // end namespace graph_analysis
+#endif // GRAPH_ANALYSIS_VERTEX_ITERATOR_HPP
