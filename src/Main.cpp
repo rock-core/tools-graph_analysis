37bac44b5a9746cba1270e21c419d0d4ea78809f
Thomas Roehr <thomas.roehr@dfki.de>
2014-08-05 09:39:20 +0200

    Add elasticnode example from qt
    http://qt-project.org/doc/qt-5/qtwidgets-graphicsview-elasticnodes-example.html

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <omviz/Dummy.hpp>
 
 int main(int argc, char** argv)
 {
-	omviz::DummyClass dummyClass;
-	dummyClass.welcome();
-
 	return 0;
 }
--- test/test_Dummy.cpp
+++ test/test_Dummy.cpp
@@ -1,10 +1,7 @@
 #include <boost/test/unit_test.hpp>
-#include <omviz/Dummy.hpp>
 
-using namespace omviz;
+//using namespace omviz;
 
 BOOST_AUTO_TEST_CASE(it_should_not_crash_when_welcome_is_called)
 {
-    omviz::DummyClass dummy;
-    dummy.welcome();
 }
