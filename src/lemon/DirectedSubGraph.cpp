97556f5f4b2738c9f3d501ed6828ac1090145a70
Thomas Roehr <thomas.roehr@dfki.de>
2015-04-04 20:04:52 +0200

    lemon: split Graph to DirectedGraph and DirectedSubGraph

---
+++ src/lemon/DirectedSubGraph.cpp
@@ -0,0 +1,70 @@
+#include "DirectedSubGraph.hpp"
+#include "DirectedGraph.hpp"
+
+namespace graph_analysis {
+namespace lemon {
+
+DirectedSubGraph::DirectedSubGraph(DirectedGraph::Ptr graph)
+    : SubGraphImpl(graph, new vertex_filter_t(graph->raw()), new edge_filter_t(graph->raw()))
+    , mpDirectedGraph(graph)
+{
+    // graph_t refers to the given subgraph
+    // needs to be explicetly set
+    setSubgraph( new graph_t(mpDirectedGraph->raw(), getVertexFilter(), getEdgeFilter()) );
+}
+
+void DirectedSubGraph::enable(Vertex::Ptr vertex)
+{
+    GraphElementId graphElementId = vertex->getId( mpDirectedGraph->getId() );
+    ::lemon::ListDigraph::Node n = mpDirectedGraph->raw().nodeFromId( graphElementId );
+
+    raw().enable(n);
+}
+
+void DirectedSubGraph::disable(Vertex::Ptr vertex)
+{
+    GraphElementId graphElementId = vertex->getId( mpDirectedGraph->getId() );
+    ::lemon::ListDigraph::Node node = mpDirectedGraph->raw().nodeFromId( graphElementId );
+    raw().disable(node);
+
+    EdgeIterator::Ptr edgeIt = getBaseGraph()->getEdgeIterator(vertex);
+    while(edgeIt->next())
+    {
+        Edge::Ptr edge = edgeIt->current();
+        disable(edge);
+    }
+}
+
+void DirectedSubGraph::enable(Edge::Ptr edge)
+{
+    GraphElementId graphElementId = edge->getId( mpDirectedGraph->getId() );
+    ::lemon::ListDigraph::Arc arc = mpDirectedGraph->raw().arcFromId( graphElementId );
+
+    raw().enable(arc);
+}
+
+void DirectedSubGraph::disable(Edge::Ptr edge)
+{
+    GraphElementId graphElementId = edge->getId( mpDirectedGraph->getId() );
+    ::lemon::ListDigraph::Arc arc = mpDirectedGraph->raw().arcFromId( graphElementId );
+
+    raw().disable(arc);
+}
+
+bool DirectedSubGraph::enabled(Vertex::Ptr vertex) const
+{
+    GraphElementId graphElementId = vertex->getId( mpDirectedGraph->getId() );
+    ::lemon::ListDigraph::Node n = mpDirectedGraph->raw().nodeFromId( graphElementId );
+    bool status = raw().status(n);
+    return status;
+}
+
+bool DirectedSubGraph::enabled(Edge::Ptr edge) const
+{
+    GraphElementId graphElementId = edge->getId( mpDirectedGraph->getId() );
+    ::lemon::ListDigraph::Arc arc = mpDirectedGraph->raw().arcFromId( graphElementId );
+    return raw().status(arc);
+}
+
+} // end namespace lemon
+} // end namespace graph_analysis
---
+++ src/lemon/DirectedSubGraph.hpp
@@ -0,0 +1,40 @@
+#ifndef GRAPH_ANALYSIS_LEMON_DIRECTED_SUB_GRAPH_HPP
+#define GRAPH_ANALYSIS_LEMON_DIRECTED_SUB_GRAPH_HPP
+
+#include <graph_analysis/SubGraphImpl.hpp>
+#include <graph_analysis/Graph.hpp>
+#include <graph_analysis/lemon/NodeIterator.hpp>
+#include <graph_analysis/lemon/ArcIterator.hpp>
+
+#include <lemon/adaptors.h>
+#include <lemon/list_graph.h>
+
+namespace graph_analysis {
+namespace lemon {
+
+class DirectedGraph;
+
+typedef ::lemon::SubDigraph< ::lemon::ListDigraph, ::lemon::ListDigraph::NodeMap<bool>, ::lemon::ListDigraph::ArcMap<bool> > SubGraphLemon;
+
+/**
+ * A subgraph that provides a subset of an existing graph
+ */
+class DirectedSubGraph : public SubGraphImpl< SubGraphLemon, ::lemon::ListDigraph::NodeMap<bool>, ::lemon::ListDigraph::ArcMap<bool> >
+{
+    boost::shared_ptr<DirectedGraph> mpDirectedGraph;
+public:
+    DirectedSubGraph(boost::shared_ptr<DirectedGraph> graph);
+
+    void enable(Vertex::Ptr vertex);
+    void disable(Vertex::Ptr vertex);
+
+    void enable(Edge::Ptr edge);
+    void disable(Edge::Ptr edge);
+
+    bool enabled(Vertex::Ptr vertex) const;
+    bool enabled(Edge::Ptr vertex) const;
+};
+
+} // end namespace lemon
+} // end namespace graph_analysis
+#endif // GRAPH_ANALYSIS_LEMON_DIRECTED_SUB_GRAPH_HPP
