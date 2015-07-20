50b73034f2d6b1faa53df2a6bc2a1d6fa103ea4d
Vlad Frasineanu <vladcf@yahoo.com>
2015-07-20 10:54:47 +0200

    Test for complete graphs

--- test/algorithms/test_CorrelationClustering.cpp
+++ test/algorithms/test_CorrelationClustering.cpp
@@ -16,74 +16,34 @@ double getWeight(Edge::Ptr edge)
     return weightedEdge->getWeight();
 }
 
-BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_1)
+// complete graph with 3 vertices
+BOOST_AUTO_TEST_CASE(cluster_correlation_1)
 {
     graph_analysis::BaseGraph::Ptr graph(new graph_analysis::lemon::DirectedGraph());
 
     Vertex::Ptr v0( new Vertex("0"));
     Vertex::Ptr v1( new Vertex("1"));
     Vertex::Ptr v2( new Vertex("2"));
-    Vertex::Ptr v3( new Vertex("3"));
-    Vertex::Ptr v4( new Vertex("4"));
-    Vertex::Ptr v5( new Vertex("5"));
 
     graph->addVertex(v0);
     graph->addVertex(v1);
     graph->addVertex(v2);
-    graph->addVertex(v3);
-    graph->addVertex(v4);
-    graph->addVertex(v5);
 
-    WeightedEdge::Ptr e0(new WeightedEdge(2.0));
+    WeightedEdge::Ptr e0(new WeightedEdge(1.0));
     e0->setSourceVertex(v0);
     e0->setTargetVertex(v1);
 
-    WeightedEdge::Ptr e1(new WeightedEdge(-5.0));
+    WeightedEdge::Ptr e1(new WeightedEdge(1.0));
     e1->setSourceVertex(v1);
     e1->setTargetVertex(v2);
 
-    WeightedEdge::Ptr e2(new WeightedEdge(-5.0));
+    WeightedEdge::Ptr e2(new WeightedEdge(-2.0));
     e2->setSourceVertex(v2);
-    e2->setTargetVertex(v3);
-
-    WeightedEdge::Ptr e3(new WeightedEdge(-1.0));
-    e3->setSourceVertex(v3);
-    e3->setTargetVertex(v0);
-
-    WeightedEdge::Ptr e4(new WeightedEdge(2.0));
-    e4->setSourceVertex(v3);
-    e4->setTargetVertex(v4);    
-
-    WeightedEdge::Ptr e5(new WeightedEdge(5.0));
-    e5->setSourceVertex(v2);
-    e5->setTargetVertex(v4);
-
-    WeightedEdge::Ptr e6(new WeightedEdge(5.0));
-    e6->setSourceVertex(v4);
-    e6->setTargetVertex(v5);
-
-    WeightedEdge::Ptr e7(new WeightedEdge(1.0));
-    e7->setSourceVertex(v5);
-    e7->setTargetVertex(v0);
-
-    WeightedEdge::Ptr e8(new WeightedEdge(2.0));
-    e8->setSourceVertex(v1);
-    e8->setTargetVertex(v5);
-
-    WeightedEdge::Ptr e9(new WeightedEdge(-5.0));
-    e9->setSourceVertex(v5);
-    e9->setTargetVertex(v2);
-
+    e2->setTargetVertex(v0);
+    
     graph->addEdge(e0);
     graph->addEdge(e1);
     graph->addEdge(e2);
-    graph->addEdge(e3);
-    graph->addEdge(e4);
-    graph->addEdge(e5);
-    graph->addEdge(e6);
-    graph->addEdge(e7);
-    graph->addEdge(e8);
-    graph->addEdge(e9);
 
     CorrelationClustering cc(graph, getWeight);
     {
@@ -98,8 +58,9 @@ BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_1)
     }
 }
 
-// test for complete graph
-BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_2)
+
+// complete graph with 4 vertices
+BOOST_AUTO_TEST_CASE(cluster_correlation_2)
 {
     graph_analysis::BaseGraph::Ptr graph(new graph_analysis::lemon::DirectedGraph());
 
@@ -158,7 +119,8 @@ BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_2)
     }
 }
 
-BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_3)
+// complete graph with 5 vertices
+BOOST_AUTO_TEST_CASE(cluster_correlation_3)
 {
     graph_analysis::BaseGraph::Ptr graph(new graph_analysis::lemon::DirectedGraph());
 
@@ -167,42 +129,52 @@ BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_3)
     Vertex::Ptr v2( new Vertex("2"));
     Vertex::Ptr v3( new Vertex("3"));
     Vertex::Ptr v4( new Vertex("4"));
-    Vertex::Ptr v5( new Vertex("5"));
 
     graph->addVertex(v0);
     graph->addVertex(v1);
     graph->addVertex(v2);
     graph->addVertex(v3);
     graph->addVertex(v4);
-    graph->addVertex(v5);
 
-    WeightedEdge::Ptr e0(new WeightedEdge(10.0));
+    WeightedEdge::Ptr e0(new WeightedEdge(5.0));
     e0->setSourceVertex(v0);
     e0->setTargetVertex(v1);
 
-    WeightedEdge::Ptr e1(new WeightedEdge(1.0));
+    WeightedEdge::Ptr e1(new WeightedEdge(-5.0));
     e1->setSourceVertex(v0);
     e1->setTargetVertex(v2);
 
-    WeightedEdge::Ptr e2(new WeightedEdge(1.0));
+    WeightedEdge::Ptr e2(new WeightedEdge(-7.0));
     e2->setSourceVertex(v0);
     e2->setTargetVertex(v3);
 
-    WeightedEdge::Ptr e3(new WeightedEdge(1.0));
+    WeightedEdge::Ptr e3(new WeightedEdge(-6.0));
     e3->setSourceVertex(v0);
     e3->setTargetVertex(v4);
 
     WeightedEdge::Ptr e4(new WeightedEdge(-3.0));
-    e4->setSourceVertex(v0);
-    e4->setTargetVertex(v5);    
+    e4->setSourceVertex(v1);
+    e4->setTargetVertex(v2);    
 
     WeightedEdge::Ptr e5(new WeightedEdge(-3.0));
-    e5->setSourceVertex(v3);
-    e5->setTargetVertex(v4);
+    e5->setSourceVertex(v1);
+    e5->setTargetVertex(v3);
 
-    WeightedEdge::Ptr e6(new WeightedEdge(2.0));
-    e6->setSourceVertex(v4);
-    e6->setTargetVertex(v5);
+    WeightedEdge::Ptr e6(new WeightedEdge(-4.0));
+    e6->setSourceVertex(v1);
+    e6->setTargetVertex(v4);
+
+    WeightedEdge::Ptr e7(new WeightedEdge(1.0));
+    e7->setSourceVertex(v2);
+    e7->setTargetVertex(v3);
+
+    WeightedEdge::Ptr e8(new WeightedEdge(5.0));
+    e8->setSourceVertex(v2);
+    e8->setTargetVertex(v4);
+
+    WeightedEdge::Ptr e9(new WeightedEdge(3.0));
+    e9->setSourceVertex(v3);
+    e9->setTargetVertex(v4);
 
     graph->addEdge(e0);
     graph->addEdge(e1);
@@ -211,6 +183,9 @@ BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_3)
     graph->addEdge(e4);
     graph->addEdge(e5);
     graph->addEdge(e6);
+    graph->addEdge(e7);
+    graph->addEdge(e8);
+    graph->addEdge(e9);
 
     CorrelationClustering cc(graph, getWeight);
     {
@@ -243,7 +218,7 @@ BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_3)
  vertices; then we can not have 3 edges 0 and the other one 1 
 */
 
-BOOST_AUTO_TEST_CASE(it_should_compute_cluster_correlation_4)
+BOOST_AUTO_TEST_CASE(cluster_correlation_4)
 {
     graph_analysis::BaseGraph::Ptr graph(new graph_analysis::lemon::DirectedGraph());
 
