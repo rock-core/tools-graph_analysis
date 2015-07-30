8ba716e106dab3ab54595c3ee1164f0b50af85d0
Petre Munteanu <clasianvmk@gmail.com>
2015-07-30 13:16:13 +0200

    documented class CustomDialog header and code

--- src/gui/CustomDialog.hpp
+++ src/gui/CustomDialog.hpp
@@ -6,12 +6,20 @@
 namespace graph_analysis {
 namespace gui {
 
+/**
+ * \class CustomDialog
+ * \file CustomDialog.hpp
+ * \brief Specialized QWidget for the properties/commnand panel; implements a dockable widget with keys (and keys combinations) listener
+ */
 class CustomDialog : public QDockWidget
 {
 public:
+    /// empty constructor
     CustomDialog();
+    /// destructor
     ~CustomDialog() {}
 protected:
+    /// qt keys press callback
     void keyPressEvent(QKeyEvent *event);
 };
 
