4478139224816c6832f3b459d94ec17bc4528b7a
Petre Munteanu <clasianvmk@gmail.com>
2015-07-31 11:40:37 +0200

    documented class SwapPortsDialog header and code

--- src/gui/SwapPortsDialog.cpp
+++ src/gui/SwapPortsDialog.cpp
@@ -19,48 +19,53 @@ void SwapPortsDialog::setupUi(QDialog *Dialog)
     if (Dialog->objectName().isEmpty())
         Dialog->setObjectName(QString::fromUtf8("Dialog"));
     Dialog->resize(441, 169);
-    buttonBox = new QDialogButtonBox(Dialog);
-    buttonBox->setObjectName(QString::fromUtf8("buttonBox"));
-    buttonBox->setGeometry(QRect(230, 120, 181, 32));
-    buttonBox->setOrientation(Qt::Horizontal);
-    buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
-    label = new QLabel(Dialog);
-    label->setObjectName(QString::fromUtf8("label"));
-    label->setGeometry(QRect(20, 16, 91, 31));
-    label_2 = new QLabel(Dialog);
-    label_2->setObjectName(QString::fromUtf8("label_2"));
-    label_2->setGeometry(QRect(20, 70, 91, 31));
+    mpButtonBox = new QDialogButtonBox(Dialog);
+    mpButtonBox->setObjectName(QString::fromUtf8("mpButtonBox"));
+    mpButtonBox->setGeometry(QRect(230, 120, 181, 32));
+    mpButtonBox->setOrientation(Qt::Horizontal);
+    mpButtonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+    mpPort1Label = new QLabel(Dialog);
+    mpPort1Label->setObjectName(QString::fromUtf8("mpPort1Label"));
+    mpPort1Label->setGeometry(QRect(20, 16, 91, 31));
+    mpPort2Label = new QLabel(Dialog);
+    mpPort2Label->setObjectName(QString::fromUtf8("mpPort2Label"));
+    mpPort2Label->setGeometry(QRect(20, 70, 91, 31));
 #ifndef LABEL_SWAPPING
-    warning = new QLabel(Dialog);
-    warning->setObjectName(QString::fromUtf8("warning"));
-    warning->setGeometry(QRect(20, 105, 70, 31));
-    warningText = new QLabel(Dialog);
-    warningText->setObjectName(QString::fromUtf8("warning"));
-    warningText->setGeometry(QRect(20, 130, 200, 31));
+    // warning note gets displayed only if the ports swap without their adjacent edges (deprecated)
+    mpWarningLabel = new QLabel(Dialog);
+    mpWarningLabel->setObjectName(QString::fromUtf8("mpWarningLabel"));
+    mpWarningLabel->setGeometry(QRect(20, 105, 70, 31));
+    mpWarningTextLabel = new QLabel(Dialog);
+    mpWarningTextLabel->setObjectName(QString::fromUtf8("warning"));
+    mpWarningTextLabel->setGeometry(QRect(20, 130, 200, 31));
 #endif
-    comboBox1 = new QComboBox(Dialog);
+    // populating the first drop-down box with all the ports
+    mpComboBox1 = new QComboBox(Dialog);
     foreach(NodeItem::VTuple tuple, mpNodeItem->getVertices())
     {
         std::string item = boost::lexical_cast<std::string>(tuple.first) + ": " + tuple.second->getLabel();
-        comboBox1->addItem(QString(item.c_str()));
+        mpComboBox1->addItem(QString(item.c_str()));
     }
-    comboBox1->setObjectName(QString::fromUtf8("comboBox1"));
-    comboBox1->setGeometry(QRect(117, 16, 291, 27));
+    mpComboBox1->setObjectName(QString::fromUtf8("mpComboBox1"));
+    mpComboBox1->setGeometry(QRect(117, 16, 291, 27));
 
-    comboBox2 = new QComboBox(Dialog);
+    // populating the second drop-down box with all the ports
+    mpComboBox2 = new QComboBox(Dialog);
     foreach(NodeItem::VTuple tuple, mpNodeItem->getVertices())
     {
         std::string item = boost::lexical_cast<std::string>(tuple.first) + ": " + tuple.second->getLabel();
-        comboBox2->addItem(QString(item.c_str()));
+        mpComboBox2->addItem(QString(item.c_str()));
     }
-    comboBox2->setObjectName(QString::fromUtf8("comboBox2"));
-    comboBox2->setGeometry(QRect(117, 70, 291, 27));
+    mpComboBox2->setObjectName(QString::fromUtf8("mpComboBox2"));
+    mpComboBox2->setGeometry(QRect(117, 70, 291, 27));
 
+    // attributing relevant titles to be displayed in the scene (e.g. buttons labels)
     retranslateUi(Dialog);
-    QObject::connect(buttonBox, SIGNAL(accepted()), this, SLOT(nodeAccept()));
-    QObject::connect(buttonBox, SIGNAL(rejected()), this, SLOT(nodeReject()));
-    QObject::connect(buttonBox, SIGNAL(accepted()), Dialog, SLOT(accept()));
-    QObject::connect(buttonBox, SIGNAL(rejected()), Dialog, SLOT(reject()));
+    // establishing signal-slot connections
+    QObject::connect(mpButtonBox, SIGNAL(accepted()), this, SLOT(swapAccept()));
+    QObject::connect(mpButtonBox, SIGNAL(rejected()), this, SLOT(swapReject()));
+    QObject::connect(mpButtonBox, SIGNAL(accepted()), Dialog, SLOT(accept()));
+    QObject::connect(mpButtonBox, SIGNAL(rejected()), Dialog, SLOT(reject()));
 
     QMetaObject::connectSlotsByName(Dialog);
 } // setupUi
@@ -68,22 +73,23 @@ void SwapPortsDialog::setupUi(QDialog *Dialog)
 void SwapPortsDialog::retranslateUi(QDialog *Dialog)
 {
     Dialog -> setWindowTitle(QApplication::translate("Dialog", "Swap Ports", 0, QApplication::UnicodeUTF8));
-    label  -> setText(QApplication::translate("Dialog", "Port ID:", 0, QApplication::UnicodeUTF8));
-    label_2-> setText(QApplication::translate("Dialog", "Port ID:", 0, QApplication::UnicodeUTF8));
+    mpPort1Label    -> setText(QApplication::translate("Dialog", "Port ID:", 0, QApplication::UnicodeUTF8));
+    mpPort2Label    -> setText(QApplication::translate("Dialog", "Port ID:", 0, QApplication::UnicodeUTF8));
 #ifndef LABEL_SWAPPING
-    warning-> setText(QApplication::translate("Dialog", "Warning:", 0, QApplication::UnicodeUTF8));
-    warningText -> setText(QApplication::translate("Dialog", "Edges are not affected by the change!", 0, QApplication::UnicodeUTF8));
+    // warning note gets displayed only if the ports swap without their adjacent edges (deprecated)
+    mpWarningLabel      -> setText(QApplication::translate("Dialog", "Warning:", 0, QApplication::UnicodeUTF8));
+    mpWarningTextLabel  -> setText(QApplication::translate("Dialog", "Edges are not affected by the change!", 0, QApplication::UnicodeUTF8));
 #endif
 } // retranslateUi
 
-void SwapPortsDialog::nodeAccept()
+void SwapPortsDialog::swapAccept()
 {
     mValid = true;
-    mPort1ID  = comboBox1->currentText().toStdString();
-    mPort2ID  = comboBox2->currentText().toStdString();
+    mPort1ID  = mpComboBox1->currentText().toStdString();
+    mPort2ID  = mpComboBox2->currentText().toStdString();
 }
 
-void SwapPortsDialog::nodeReject()
+void SwapPortsDialog::swapReject()
 {
     mValid = false;
 }
--- src/gui/SwapPortsDialog.hpp
+++ src/gui/SwapPortsDialog.hpp
@@ -29,37 +29,62 @@ namespace gui {
 
     class NodeItem;
 
+/**
+ * \class SwapPortsDialog
+ * \file SwapPortsDialog.hpp
+ * \brief pop-up dialog manager for swapping two ports of a cluster node
+ */
 class SwapPortsDialog : public QObject
 {
     Q_OBJECT
 public:
+    /// constructor; stores the concerned cluster node reference
     SwapPortsDialog(NodeItem *nodeItem);
+    /// destructor
     ~SwapPortsDialog(){}
+    /// sets up the provided qt dialog instance
     void setupUi(QDialog *Dialog);
+    /// sets up visible titles for the scene (e.g. buttons labels)
     void retranslateUi(QDialog *Dialog);
+    /// getter method for boolean validation witness
     bool isValid() { return mValid; }
+    /// getter method for the first port ID dialog result
     std::string getPort1ID () { return mPort1ID;  }
+    /// getter method for the second port ID dialog result
     std::string getPort2ID () { return mPort2ID;  }
 
 public slots:
-    void nodeAccept();
-    void nodeReject();
+    /// slot for listening for whenever the user presses "Ok" in the resulting pop-up dialog window
+    void swapAccept();
+    /// slot for listening for whenever the user presses "Cancel" (or ESC/close window corner button) in the resulting pop-up dialog window
+    void swapReject();
 
 private:
+    /// main qt dialog instance to be used
     QDialog mDialog;
-    std::string mNewLabel;
+    /// stores the chosen first port ID as text user-interaction result
     std::string mPort1ID;
+    /// stores the chosen second port ID as text user-interaction result
     std::string mPort2ID;
 
+    /// concerned cluster node
     NodeItem *mpNodeItem;
+    /// boolean witness for validation; true whenever the user-interaction ends with the "Ok" button being pressed; false otherwise
     bool mValid;
-    QDialogButtonBox *buttonBox;
-    QLabel *label;
-    QLabel *label_2;
-    QLabel *warning;
-    QLabel *warningText;
-    QComboBox *comboBox1;
-    QComboBox *comboBox2;
+    /// in-scene set of buttons for this pop-window ("Ok" and "Cancel")
+    QDialogButtonBox *mpButtonBox;
+    /// in scene label displaying "Port ID:"
+    QLabel *mpPort1Label;
+    /// in scene label displaying "Port ID:"
+    QLabel *mpPort2Label;
+    /// in scene label displaying "Warning:" in case full label swapping is not supported (i.e. edges are not affected by swapping - deprecated)
+    QLabel *mpWarningLabel;
+    /// in scene label displaying the warning message in case full label swapping is not supported (i.e. edges are not affected by swapping - deprecated)
+    QLabel *mpWarningTextLabel;
+    /// GUI element from which the user selects the first desired port for swapping
+    QComboBox *mpComboBox1;
+    /// GUI element from which the user selects the second desired port for swapping
+    QComboBox *mpComboBox2;
 };
 
 } // end namespace gui
