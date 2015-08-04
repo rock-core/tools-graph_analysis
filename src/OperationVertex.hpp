4da8e18e15660d0fbc7385d6fed3e05ef4b69452
Petre Munteanu <clasianvmk@gmail.com>
2015-08-04 14:49:47 +0200

    introduce 2 new Vertex derived classes: PropertyVertex and OperationVertex

---
+++ src/OperationVertex.hpp
@@ -0,0 +1,34 @@
+#ifndef GRAPH_ANALYSIS_OPERATIONVERTEX_HPP
+#define GRAPH_ANALYSIS_OPERATIONVERTEX_HPP
+
+#include <iostream>
+#include <graph_analysis/Vertex.hpp>
+#include <graph_analysis/GraphElement.hpp>
+
+namespace graph_analysis
+{
+
+/**
+ * \brief An Operation vertex inherited to allow storing data of Operation type
+ * \details For the purpose of storing data with a vertex, this storage type inherits
+ * from class Vertex
+ */
+class OperationVertex : public Vertex
+{
+public:
+    OperationVertex(const std::string& label = std::string());
+    /** Get class name
+     * \return class name
+     */
+    virtual std::string getClassName() const { return "graph_analysis::OperationVertex"; }
+
+protected:
+    /**
+     * Create a copy of this vertex
+     */
+    virtual OperationVertex* getClone() const { return new OperationVertex(*this); }
+
+};
+
+} // end namespace graph_analysis
+#endif // GRAPH_ANALYSIS_OPERATIONVERTEX_HPP
---
+++ src/PropertyVertex.cpp
@@ -0,0 +1,9 @@
+#include "PropertyVertex.hpp"
+
+namespace graph_analysis {
+
+PropertyVertex::PropertyVertex(const std::string& label)
+    : Vertex(label)
+{}
+
+} // end namespace graph_analysis
