2098fab300c8c952aa9dcbf0d8da0a19cd37ecfa
Thomas Roehr <thomas.roehr@dfki.de>
2015-08-26 12:05:32 +0200

    Add NWeightedEdge

---
+++ src/NWeightedEdge.hpp
@@ -0,0 +1,111 @@
+#ifndef GRAPH_ANALYSIS_NWEIGHTED_EDGE_HPP
+#define GRAPH_ANALYSIS_NWEIGHTED_EDGE_HPP
+
+#include <iostream>
+#include <graph_analysis/Edge.hpp>
+
+namespace graph_analysis {
+
+/**
+ * \brief NWeightedEdge is an Edge which can be associated with N-weights, i.e.,
+ * a vector of weights
+ */
+template<typename T, size_t Dim = 2>
+class NWeightedEdge : public Edge
+{
+public:
+    typedef boost::shared_ptr< NWeightedEdge > Ptr;
+
+    NWeightedEdge(const std::vector<T>& weights)
+        : Edge()
+        , mWeights(weights)
+    {
+        validateDimensions(mWeights);
+    }
+
+    NWeightedEdge(Vertex::Ptr source, Vertex::Ptr target, const std::vector<T>& weights)
+        : Edge(source, target)
+        , mWeights(weights)
+    {
+        validateDimensions(mWeights);
+    }
+
+    virtual ~NWeightedEdge() {}
+
+    // Get class name
+    // \return class name
+    virtual std::string getClassName() const { return "graph_analysis::NWeightedEdge"; }
+
+    /**
+     * Convert element to string
+     */
+    virtual std::string toString() const
+    {
+        std::stringstream ss;
+        ss << Edge::toString();
+        ss << ": " << "[";
+        typename std::vector<T>::const_iterator cit = mWeights.begin();
+        for(;; ++cit)
+        {
+            ss << *cit;
+            if(cit + 1 == mWeights.end())
+            {
+                break;
+            } else {
+                ss << ",";
+            }
+        }
+        ss << "]";
+        return ss.str();
+    }
+
+    void setWeights(const std::vector<T>& weights)
+    {
+        validateDimensions(weights);
+        mWeights = weights;
+    }
+
+    std::vector<T> getWeights() const { return mWeights; }
+
+    void setWeight(size_t index, T value)
+    {
+        try {
+            mWeights.at(index) = value;
+        } catch(const std::out_of_range& e)
+        {
+            throw std::out_of_range("graph_analysis::NWeightedEdge::getWeights: " + std::string(e.what()) );
+        }
+    }
+
+    /**
+     * Get weight at given index (dimension)
+     */
+    const T& getWeight(size_t index) const
+    {
+        try {
+            return mWeights.at(index);
+        } catch(const std::out_of_range& e)
+        {
+            throw std::out_of_range("graph_analysis::NWeightedEdge::getWeight: " + std::string(e.what()) );
+        }
+    }
+
+protected:
+
+    void validateDimensions(const std::vector<T>& weights) const
+    {
+        if(weights.size() != Dim)
+        {
+            std::stringstream ss;
+            ss << "graph_analysis::NWeightedEdge: invalid number of weights: expected " << Dim << " got " << weights.size();
+            throw std::invalid_argument(ss.str());
+        }
+    }
+
+    virtual Edge* getClone() const { return new NWeightedEdge(*this); }
+
+    std::vector<T> mWeights;
+};
+
+} // end namespace graph_analysis
+#endif // GRAPH_ANALYSIS_NWEIGHTED_EDGE_HPP
--- test/test_EdgesVertices.cpp
+++ test/test_EdgesVertices.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test.hpp>
 #include <graph_analysis/Edge.hpp>
 #include <graph_analysis/WeightedEdge.hpp>
+#include <graph_analysis/NWeightedEdge.hpp>
 #include <graph_analysis/Vertex.hpp>
 
 using namespace graph_analysis;
@@ -22,13 +23,46 @@ BOOST_AUTO_TEST_CASE(clone)
     double weight = 10.0;
     Edge::Ptr edge0(new WeightedEdge(weight));
     Edge::Ptr edge0_clone = edge0->clone();
-    
+
     WeightedEdge::Ptr w0 = boost::dynamic_pointer_cast<WeightedEdge>(edge0);
     WeightedEdge::Ptr w1 = boost::dynamic_pointer_cast<WeightedEdge>(edge0_clone);
 
     BOOST_REQUIRE_MESSAGE(w0->getWeight() == w1->getWeight(), "Edge and clone have same weights after cloning");
     w1->setWeight(weight + 1.0);
     BOOST_REQUIRE_MESSAGE(w0->getWeight() != w1->getWeight(), "Edge and clone have different weights");
+}
+
+BOOST_AUTO_TEST_CASE(nweighted_edge)
+{
+    std::string name = "vertex-name";
+
+    Vertex::Ptr vertex0( new Vertex(name));
+    Vertex::Ptr vertex0_clone = vertex0->clone();
+
+    BOOST_REQUIRE_MESSAGE(vertex0->getLabel() == vertex0_clone->getLabel(), "Vertex clone has same name after cloning");
+    vertex0_clone->setLabel("otherLabel");
+    BOOST_REQUIRE_MESSAGE(vertex0->getLabel() != vertex0_clone->getLabel(), "Vertex clone has different name");
+
+
+    std::vector<double> weights(2);
+    weights[0] = 1.0;
+    weights[1] = 2.0;
+
+    Edge::Ptr edge0(new NWeightedEdge<double>(weights) );
+    Edge::Ptr edge0_clone = edge0->clone();
+
+    NWeightedEdge<double>::Ptr w0 = boost::dynamic_pointer_cast<NWeightedEdge<double> >(edge0);
+    NWeightedEdge<double>::Ptr w1 = boost::dynamic_pointer_cast<NWeightedEdge<double> >(edge0_clone);
+
+    BOOST_REQUIRE_MESSAGE(w0->getWeights() == w1->getWeights(), "Edge and clone have same weights after cloning");
+
+    w1->setWeight(0, w1->getWeight(0) + 1.0);
+    BOOST_REQUIRE_MESSAGE(w0->getWeights() != w1->getWeights(), "Edge and clone have different weights");
+
+    BOOST_REQUIRE_THROW(w0->getWeight(10), std::out_of_range);
+
+    std::vector<double> weights3d(3);
+    BOOST_REQUIRE_THROW( NWeightedEdge<double>::Ptr(new NWeightedEdge<double>(weights3d)), std::invalid_argument );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
