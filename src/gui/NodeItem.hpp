d690608d19cb580550e46c6c4b87e78aed01bcc2
Martin Zenzes <martin.zenzes@dfki.de>
2016-03-16 17:28:33 +0100

    NodeItem: remove "calculateForces"
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>
    
    Conflicts:
    	src/gui/NodeItem.hpp

--- src/gui/NodeItem.hpp
+++ src/gui/NodeItem.hpp
@@ -59,9 +59,6 @@ public:
     enum { Type = UserType + 1 };
     int type() const { return Type; }
 
-    /// computes the force field values for each edge (w.r.t. the force field automatic re-arranging algorithm)
-    void calculateForces();
-
     /// actuator method the same force field (automatic re-arranging) algorithm
     void advance(int phase) { if(phase == 2) advance(); }
 
