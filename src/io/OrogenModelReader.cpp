13206074a97b0b0380f5a29e76134b7b2bd98d76
Moritz Schilling <moritz.schilling@dfki.de>
2016-04-01 12:44:59 +0200

    First version of an OrogenModelImporter
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- src/io/OrogenModelReader.cpp
+++ src/io/OrogenModelReader.cpp
@@ -4,74 +4,77 @@
 #include <graph_analysis/EdgeTypeManager.hpp>
 
 #include <graph_analysis/gui/ComponentGraphEditor/Component.hpp>
-#include <graph_analysis/gui/ComponentGraphEditor/PortConnection.hpp>
+#include <graph_analysis/gui/ComponentGraphEditor/HasFeature.hpp>
+
+// Include some yaml parsing stuff
+#include <yaml-cpp/yaml.h>
 
 namespace graph_analysis {
 namespace io {
 
+    //switch (inputs.Type()) {
+        //case YAML::NodeType::Null:
+            //std::cout << "NULL";
+            //break;
+        //case YAML::NodeType::Scalar:
+            //std::cout << "SCALAR";
+            //break;
+        //case YAML::NodeType::Sequence:
+            //std::cout << "SEQ";
+            //break;
+        //case YAML::NodeType::Map:
+            //std::cout << "MAP";
+            //break;
+        //case YAML::NodeType::Undefined:
+            //std::cout << "UNDEF";
+            //break;
+        //default:
+            //std::cout << "W00t?!";
+            //break;
+    //}
+    //std::cout << std::endl;
+
+template<typename T> void createPort(const std::string &label, BaseGraph::Ptr graph, Vertex::Ptr parent)
+{
+    typename T::Ptr port = typename T::Ptr(new T(label));
+    graph->addVertex(port);
+    HasFeature::Ptr has = HasFeature::Ptr(new HasFeature(parent, port, "has"));
+    graph->addEdge(has);
+}
+
 void OrogenModelReader::read(const std::string &filename,
                              BaseGraph::Ptr graph) {
+    // handy pointers
+    //VertexTypeManager *vManager = VertexTypeManager::getInstance();
+    //EdgeTypeManager *eManager = EdgeTypeManager::getInstance();
+
     // remove previous graph
     graph->clear();
 
-    // handy pointers
-    VertexTypeManager *vManager = VertexTypeManager::getInstance();
-    EdgeTypeManager *eManager = EdgeTypeManager::getInstance();
-
-    // how this works: create a "Cluster" as top-level vertex. then create each
-    // individual input and output port. add them all to the graph:
-    Vertex::Ptr vertex1 = vManager->createVertex("graph_analysis::Component", "node1");
-    Vertex::Ptr vertex2 = vManager->createVertex("graph_analysis::Component", "node2");
-    Vertex::Ptr vertex3 = vManager->createVertex("graph_analysis::InputPort", "iPort1");
-    Vertex::Ptr vertex4 = vManager->createVertex("graph_analysis::OutputPort", "oPort");
-    Vertex::Ptr vertex5 = vManager->createVertex("graph_analysis::OutputPort", "oPort2");
-    Vertex::Ptr vertex6 = vManager->createVertex("graph_analysis::Component", "node3");
-    Vertex::Ptr vertex7 = vManager->createVertex("graph_analysis::OutputPort", "oPort2");
-    Vertex::Ptr vertex8 = vManager->createVertex("graph_analysis::InputPort", "iPort2");
-    graph->addVertex(vertex1);
-    graph->addVertex(vertex2);
-    graph->addVertex(vertex3);
-    graph->addVertex(vertex4);
-    graph->addVertex(vertex5);
-    graph->addVertex(vertex6);
-    graph->addVertex(vertex7);
-    graph->addVertex(vertex8);
-
-    // fancier
-    Component::Ptr comp = Component::Ptr(new Component("testcomp"));
+    // Load and parse the yaml file
+    YAML::Node task = YAML::LoadFile(filename);
+    YAML::const_iterator it=task.begin();
+    std::string baseClassName = it->first.as<std::string>();
+    it=it->second.begin();
+    std::string className = it->first.as<std::string>();
+    // At first create the master node representing the whole task
+    Component::Ptr comp = Component::Ptr(new Component(baseClassName + "::" + className));
     graph->addVertex(comp);
-    OutputPort::Ptr port1 = OutputPort::Ptr(new OutputPort("bla"));
-    graph->addVertex(port1);
-    graph->addEdge(
-        eManager->createEdge("graph_analysis::HasFeature", comp, port1, "has"));
-    InputPort::Ptr port2 = InputPort::Ptr(new InputPort("blupp"));
-    graph->addVertex(port2);
-    graph->addEdge(
-        eManager->createEdge("graph_analysis::HasFeature", comp, port2, "has"));
-    PortConnection::Ptr conn = PortConnection::Ptr(new PortConnection(
-        port1, dynamic_pointer_cast<InputPort>(vertex8), "testcomp"));
-    graph->addEdge(conn);
 
-    // now add the ports to the respective clusters via "hasFeature" label
-    Edge::Ptr edge1 = eManager->createEdge("graph_analysis::HasFeature",
-                                           vertex1, vertex3, "has");
-    Edge::Ptr edge2 = eManager->createEdge("graph_analysis::HasFeature",
-                                           vertex2, vertex5, "has");
-    Edge::Ptr edge3 = eManager->createEdge("graph_analysis::HasFeature",
-                                           vertex2, vertex4, "has");
-    Edge::Ptr edge4 = eManager->createEdge("graph_analysis::PortConnection",
-                                           vertex4, vertex3, "some-connection");
-
-    Edge::Ptr edge5 = eManager->createEdge("graph_analysis::HasFeature",
-                                           vertex1, vertex7, "has");
-    Edge::Ptr edge6 = eManager->createEdge("graph_analysis::HasFeature",
-                                           vertex1, vertex8, "has");
-    graph->addEdge(edge1);
-    graph->addEdge(edge2);
-    graph->addEdge(edge3);
-    graph->addEdge(edge4);
-    graph->addEdge(edge5);
-    graph->addEdge(edge6);
+    // Then we create all input ports and relate them to the master node
+    YAML::Node ports = it->second; // its a map!
+    YAML::Node inputs = ports["inputPorts"]; // its a seq!
+    for (it = inputs.begin(); it != inputs.end(); ++it)
+    {
+        YAML::Node current = *it;
+        createPort<InputPort>(current["Name"].as<std::string>(), graph, comp);
+    }
+    YAML::Node outputs = ports["outputPorts"]; // its a seq!
+    for (it = outputs.begin(); it != outputs.end(); ++it)
+    {
+        YAML::Node current = *it;
+        createPort<OutputPort>(current["Name"].as<std::string>(), graph, comp);
+    }
 }
 
 } // end namespace io
