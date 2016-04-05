f9a37623130cbfde5c32d5a6ad3a0ce623fd58eb
Moritz Schilling <moritz.schilling@dfki.de>
2016-04-05 10:40:44 +0200

    Added TaskTenmplateContainer class
    NOTE: It might be better to inherit from std::map
    
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

---
+++ src/task_graph/TaskTemplateContainer.cpp
@@ -0,0 +1,37 @@
+#include "TaskTemplateContainer.hpp"
+
+namespace graph_analysis
+{
+namespace task_graph
+{
+
+bool TaskTemplateContainer::add(TaskTemplate::Ptr templ)
+{
+    if (storage.count(templ->yamlFileName()) > 0)
+        return false;
+    else
+        storage[templ->yamlFileName()] = templ;
+    return true;
+}
+
+void TaskTemplateContainer::remove(TaskTemplate::Ptr templ)
+{
+    storage.erase(templ->yamlFileName());
+}
+
+TaskTemplate::Ptr TaskTemplateContainer::find(const std::string& name)
+{
+    std::map<std::string, TaskTemplate::Ptr>::const_iterator it;
+    if ((it = storage.find(name)) != storage.end())
+        return it->second;
+    else
+        return TaskTemplate::Ptr();
+}
+
+void TaskTemplateContainer::clear()
+{
+    storage.clear();
+}
+
+}
+}
