f61cd54ed1a352c49f0075d9763691408722be7d
Thomas Roehr <thomas.roehr@dfki.de>
2016-04-18 18:08:01 +0200

    snap: add keyword 'virtual' to addEdgeInternal

--- src/snap/DirectedGraph.hpp
+++ src/snap/DirectedGraph.hpp
@@ -86,7 +86,7 @@ public:
      * \brief Add an edge
      * \return an edge interator
      */
-    GraphElementId addEdgeInternal(const Edge::Ptr& edge, GraphElementId sourceVertexId, GraphElementId targetVertexId);
+    virtual GraphElementId addEdgeInternal(const Edge::Ptr& edge, GraphElementId sourceVertexId, GraphElementId targetVertexId);
 
     Vertex::Ptr getVertex(GraphElementId id) const;
     Edge::Ptr getEdge(GraphElementId id) const;
